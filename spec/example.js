ffcb7651d3352ee03cb9bfbbbc1cd8ded3afc5c4
Administrator <admin@example.com>
2015-08-19 09:54:29 +0200

    cache added, isMariaHimmelfahrt added

--- spec/example.js
+++ spec/example.js
@@ -2,7 +2,14 @@ var feiertagejs = require('../lib/feiertage.js');
 
 
 var feiertageBW =  feiertagejs.getHolidays(2015,'BW');
+var feiertageBY =  feiertagejs.getHolidays(2015,'BY');
 var isHolidayToday =  feiertagejs.isHoliday(new Date(),'BW');
 
-console.log(feiertageBW);
-console.log(isHolidayToday);
+
+
+//console.log(feiertageBW);
+console.log(feiertageBY);
+console.log(feiertagejs.getCache());
+
+//console.log(isHolidayToday);
+
