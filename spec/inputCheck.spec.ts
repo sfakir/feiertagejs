d67dd022832b245c58f54abe649a6c3fbb2217db
Simon Fakir <simon@fakir-it.de>
2024-06-14 10:41:59 +0200

    added unit tests to check holiday properties

--- spec/inputCheck.spec.ts
+++ spec/inputCheck.spec.ts
@@ -1,5 +1,9 @@
 import { describe, it, expect } from 'vitest';
-import { getHolidays } from '../src/feiertage';
+import {
+  getHolidayByDate,
+  getHolidays,
+  getHolidaysByDate,
+} from '../src/feiertage';
 
 describe('Wrong Inputs', () => {
   it.each([[2017], [2018]])(
@@ -10,4 +14,20 @@ describe('Wrong Inputs', () => {
       expect(result.length).toEqual(result2.length);
     },
   );
+});
+describe.only('Check if holiday properties are set', () => {
+  const holiday = getHolidayByDate(new Date(2024, (12 - 1), 25), 'BUND'); // Christmas
+
+  it('should have a name', () => {
+    expect(holiday?.name).toBeDefined();
+  });
+  it('should have a dateString', () => {
+    expect(holiday?.dateString).toBeDefined();
+  });
+  it('should have a date', () => {
+    expect(holiday?.date).toBeDefined();
+  });
+  it('should have regions', () => {
+    expect(holiday?.regions).toBeDefined();
+  });
 });
